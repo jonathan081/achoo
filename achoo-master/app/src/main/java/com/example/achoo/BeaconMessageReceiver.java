d5b9a90d42fa22376de57dd2dfb0155b623fa005
Andrew Cervantes <45413115+AJCervantes@users.noreply.github.com>
2020-11-28 09:23:48 -0500

    Add files via upload

---
+++ achoo-master/app/src/main/java/com/example/achoo/BeaconMessageReceiver.java
@@ -0,0 +1,28 @@
+package com.example.achoo;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import com.google.android.gms.nearby.Nearby;
+import com.google.android.gms.nearby.messages.Message;
+import com.google.android.gms.nearby.messages.MessageListener;
+
+public class BeaconMessageReceiver {
+    private static final String TAG = MainActivity.class.getName();
+
+    // @Override
+    public void onReceive(Context context, Intent intent) {
+        Nearby.getMessagesClient(context).handleIntent(intent, new MessageListener() {
+            @Override
+            public void onFound(Message message) {
+                Log.i(TAG, "Found message via PendingIntent: " + message);
+            }
+
+            @Override
+            public void onLost(Message message) {
+                Log.i(TAG, "Lost message via PendingIntent: " + message);
+            }
+        });
+    }
+}
